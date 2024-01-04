6cb88bba483aa82ddb1b82bf234b2c5f8b578ab2
MatexConcept <matexconcept@gmail.com>
2024-01-04 16:22:01 +0100

    updated

--- client/PageComponents/Components/Banner.jsx
+++ client/PageComponents/Components/Banner.jsx
@@ -1,6 +1,40 @@
 import React from "react";
+import Link from "next/link";
 
 const Banner = () => {
+  const category = [
+    {
+      name:"Housing",
+      link: "/housing",
+      worth: 4264.2383,
+    },
+    {
+      name:"Office",
+      link: "/office",
+      worth: 6264.2383,
+    },
+    {
+      name:"Farmhouse",
+      link: "farmhouse",
+      worth: 1264.2383,
+    },
+    {
+      name:"Rental",
+      link: "/rental",
+      worth: 9264.2383,
+    },
+    {
+      name:"Commercial",
+      link: "/commercial",
+      worth: 7264.2383,
+    },
+    {
+      name:"Country",
+      link: "/country",
+      worth: 5264.2383,
+    },
+ 
+  ]
   return (
     <div class="banner-three slider-style-3 pt--70">
       <div class="container">
@@ -12,15 +46,15 @@ const Banner = () => {
                   <a href="product-details.html">
                     <img
                       class="w-100"
-                      src="/banner/h-three-banner-01.jpg"
+                      src="/portfolio/portfolio-11.jpg"
                       alt="NFT_portfolio"
                     />
                   </a>
                   <div class="read-wrapper">
                     <h5>
-                      <a href="product-details.html">HasLivbe</a>
+                      <a href="product-details.html">@Homzyy</a>
                     </h5>
-                    <span>Md. Master</span>
+                    <span>Join Membership</span>
                   </div>
                 </div>
               </div>
@@ -29,23 +63,26 @@ const Banner = () => {
 
           <div class="col-lg-7">
             <div class="row g-4">
-              {[1, 2, 3, 4, 5, 6].map((el, i) => (
+              {category.map((el, i) => (
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                   <div class="slide-small-wrapper">
                     <div class="thumbnail thumbnail-overlay">
-                      <a href="product-details.html">
+                      <Link href={{
+                        pathname: `/category${el.link}`,
+                        query: {name: `${el.name}`},
+                      }}>
                         <img
                           class="w-100"
-                          src="/banner/banner-sm-01.jpg"
+                          src={`/portfolio/portfolio-${i + 12}.jpg`}
                           alt="Nft_Profile"
                         />
-                      </a>
+                      </Link>
                     </div>
                     <div class="read-wrapper">
                       <h5 class="title">
-                        <a href="#">HasLivbe</a>
+                        <a href="#">{el.name}</a>
                       </h5>
-                      <span>Md. Master</span>
+                      <span>{el.worth} MATIC</span>
                     </div>
                   </div>
                 </div>
--- client/PageComponents/Components/Live.jsx
+++ client/PageComponents/Components/Live.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Live = () => {
+const Live = ({ properties}) => {
   return (
     <div class="rn-live-bidding-area rn-section-gapTop">
       <div class="container">
@@ -19,119 +19,67 @@ const Live = () => {
           </div>
         </div>
         <div class="row g-5">
-          {[1, 2, 3, 4, 5].map((el, i) => (
+          { properties.map((property, i) => (
             <div
               class="col-5 col-lg-4 col-md-6 col-sm-6 col-12"
               data-sal-delay="150"
-              data-sal="slide-up"
+              data-sal=""
               data-sal-duration="800"
             >
               <div
                 class="product-style-one"
-                data-sal="slide-up"
+                data-sal=""
                 data-sal-duration="800"
                 data-sal-delay="150"
               >
                 <div class="card-thumbnail">
-                  <a href="product-details.html">
+                  <a href={`/detail?property=${property.productId}`}>
                     <img
-                      src="/portfolio/portfolio-01.jpg"
-                      alt="NFT_portfolio"
+                      src={property.image}
+                      alt={property.title}
                     />
                   </a>
-                  <div class="countdown" data-date="-12-30">
-                    <div class="countdown-container days">
-                      <span class="countdown-value">87</span>
-                      <span class="countdown-heading">D's</span>
-                    </div>
-                    <div class="countdown-container hours">
-                      <span class="countdown-value">23</span>
-                      <span class="countdown-heading">H's</span>
-                    </div>
-                    <div class="countdown-container minutes">
-                      <span class="countdown-value">38</span>
-                      <span class="countdown-heading">Min's</span>
-                    </div>
-                    <div class="countdown-container seconds">
-                      <span class="countdown-value">27</span>
-                      <span class="countdown-heading">Sec</span>
-                    </div>
-                  </div>
+                  
                 </div>
                 <div class="product-share-wrapper">
                   <div class="profile-share">
-                    <a
-                      href="author.html"
+
+                  {
+                    property.reviewers.map((el, i) => (
+                      <a
+                    
                       class="avatar"
-                      data-tooltip="John Lee"
+                      data-tooltip={`${el.slice(0, 15)}...`}
                     >
-                      <img src="/client/client-1.png" alt="Nft_Profile" />
+                      <img src={`/client/client-${i + 1}.png`} alt="Nft_Profile" />
                     </a>
-                    <a
-                      href="author.html"
-                      class="avatar"
-                      data-tooltip="Janin Ara"
-                    >
-                      <img src="/client/client-2.png" alt="Nft_Profile" />
-                    </a>
-                    <a
-                      href="author.html"
-                      class="avatar"
-                      data-tooltip="Jogn Due"
-                    >
-                      <img src="/client/client-3.png" alt="Nft_Profile" />
-                    </a>
+                    ))
+                  }
+                
+                {
+                  property.reviewers.length !== 0 && (
                     <a class="more-author-text" href="#">
-                      9+ Place Bit.
+                     Interested Users
                     </a>
+                  )
+                }
+                 
                   </div>
-                  <div class="share-btn share-btn-activation dropdown">
-                    <button
-                      class="icon"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      <svg
-                        viewBox="0 0 14 4"
-                        fill="none"
-                        width="16"
-                        height="16"
-                        class="sc-bdnxRM sc-hKFxyN hOiKLt"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          clip-rule="evenodd"
-                          d="M3.5 2C3.5 2.82843 2.82843 3.5 2 3.5C1.17157 3.5 0.5 2.82843 0.5 2C0.5 1.17157 1.17157 0.5 2 0.5C2.82843 0.5 3.5 1.17157 3.5 2ZM8.5 2C8.5 2.82843 7.82843 3.5 7 3.5C6.17157 3.5 5.5 2.82843 5.5 2C5.5 1.17157 6.17157 0.5 7 0.5C7.82843 0.5 8.5 1.17157 8.5 2ZM11.999 3.5C12.8274 3.5 13.499 2.82843 13.499 2C13.499 1.17157 12.8274 0.5 11.999 0.5C11.1706 0.5 10.499 1.17157 10.499 2C10.499 2.82843 11.1706 3.5 11.999 3.5Z"
-                          fill="currentColor"
-                        ></path>
-                      </svg>
-                    </button>
-                    <div class="share-btn-setting dropdown-menu dropdown-menu-end">
-                      <button
-                        type="button"
-                        class="btn-setting-text share-text"
-                        data-bs-toggle="modal"
-                        data-bs-target="#shareModal"
-                      >
-                        Share
-                      </button>
-                      <button
-                        type="button"
-                        class="btn-setting-text report-text"
-                        data-bs-toggle="modal"
-                        data-bs-target="#reportModal"
-                      >
-                        Report
-                      </button>
-                    </div>
-                  </div>
+                
                 </div>
                 <a href="product-details.html">
-                  <span class="product-name">Preatent</span>
+                  <span class="product-name">
+                    {
+                      property.title.length >= 25 ? `${property.title.slice(0, 22 )}...` :
+                      property.title
+                    }
+                  </span>
                 </a>
-                <span class="latest-bid">Highest bid 1/20</span>
+                <span class="latest-bid">
+                Category: {property.category}
+                </span>
                 <div class="bid-react-area">
-                  <div class="last-bid">0.244wETH</div>
+                  <div class="last-bid">{property.price} MATIC</div>
                   <div class="react-area">
                     <svg
                       viewBox="0 0 17 16"
@@ -146,12 +94,16 @@ const Live = () => {
                         stroke-width="2"
                       ></path>
                     </svg>
-                    <span class="number">322</span>
+                    <span class="number">
+                      {
+                        property.reviewers.length
+                      }
+                    </span>
                   </div>
                 </div>
               </div>
             </div>
-          ))}
+          )).slice(0, 5)}
         </div>
       </div>
     </div>
